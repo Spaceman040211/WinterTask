116963e89c26002e56030e7db7672abc5a0f8911
Spaceman04011 <2404842048@qq.com>
2023-01-14 19:14:01 +0800

    [feature]一个WanAndroid的开始

---
+++ app/src/main/java/com/example/progresseveryday/view/WechatArticleView.java
@@ -0,0 +1,7 @@
+package com.example.progresseveryday.view;
+
+import com.example.progresseveryday.model.WechatArticleModel;
+
+public interface WechatArticleView {
+    void getWechatArticleData(WechatArticleModel model);
+}
---
+++ app/src/main/java/com/example/progresseveryday/view/WechatListView.java
@@ -0,0 +1,8 @@
+package com.example.progresseveryday.view;
+
+import com.example.progresseveryday.model.WechatListModel;
+
+public interface WechatListView {
+
+    void getWechartListData(WechatListModel model);
+}
