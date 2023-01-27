0b80c0999e85fd09c38c047535b8bd8d981793e1
Spaceman04011 <2404842048@qq.com>
2023-01-27 19:46:18 +0800

    [feature]一个Android的开始

--- app/src/main/java/com/example/progresseveryday/activity/BaseActivity.java
+++ app/src/main/java/com/example/progresseveryday/activity/BaseActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.progresseveryday.utils.ActivityManager;
-import com.example.progresseveryday.utils.StatusBarUtil;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -30,16 +29,11 @@ public class BaseActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );*/
 
-        setStatusBar();
 
         mContext = this;
 
     }
-    protected void setStatusBar() {
-        //这里做了两件事情:1.使状态栏透明并使contentView填充到状态栏
-        // 2.预留出状态栏的位置，防止界面上的控件离顶部靠的太近。这样就可以实现开头说的第二种情况的沉浸式状态栏了
-        StatusBarUtil.StatusBarUtil.setTransparent(this);
-    }
+
 
 
     @Override
