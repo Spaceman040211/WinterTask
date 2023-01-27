03dc3b90dbd669d4028459c6975598082ac9964f
Spaceman04011 <2404842048@qq.com>
2023-01-27 21:58:47 +0800

    [feature]一个Android的开始

---
+++ app/src/main/java/com/example/progresseveryday/view/KnowledgeDataView.java
@@ -0,0 +1,15 @@
+package com.example.progresseveryday.view;
+
+
+import com.example.progresseveryday.model.KnowledgeModel;
+
+/**
+ * </pre>
+ *     desc:知识体系数据 view
+ * </pre>
+ */
+
+public interface KnowledgeDataView {
+    void getSystemTreeData(KnowledgeModel model);
+
+}
---
+++ app/src/main/java/com/example/progresseveryday/view/RegisterView.java
@@ -0,0 +1,7 @@
+package com.example.progresseveryday.view;
+
+import com.example.progresseveryday.model.RegisterModel;
+
+public interface RegisterView {
+    void getRegisterData(RegisterModel model);
+}
