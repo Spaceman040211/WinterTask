1f1d1e80dbe895d94215ac5625dd37105f4a5ae2
Spaceman04011 <2404842048@qq.com>
2023-01-28 23:55:43 +0800

    [bug]依赖修改

--- app/src/main/java/com/example/progresseveryday/fragment/HomeFragment.java
+++ app/src/main/java/com/example/progresseveryday/fragment/HomeFragment.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SimpleItemAnimator;
 
-import android.support.v4.widget.NestedScrollView;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SimpleItemAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
